525337ee626c19c3bf8c09a4c3be26b9c1647272
Luciano Germani <lucianogermani93@gmail.com>
2025-05-15 12:36:50 +0200

    Feat: Add persistent store zustand

--- src/store.ts
+++ src/store.ts
@@ -1,58 +1,63 @@
 
-import {create} from 'zustand';
-//Necesario para ver zustand en Redux Devtools
-import {devtools} from 'zustand/middleware'
-import {v4 as uuidv4} from 'uuid'
+import { create } from 'zustand';
+//devtools: Necesario para ver zustand en Redux Devtools / persist: Necesario para estado persistente
+import { createJSONStorage, devtools, persist } from 'zustand/middleware'
+import { v4 as uuidv4 } from 'uuid'
 import type { DraftPatient, Patient } from './types';
 
 type PatientState = {
-    patients: Patient[],
-    activeId: Patient['id']
-    addPatient:(data:DraftPatient) => void
-    deletePatient: (id:Patient['id']) => void
-    getPatientById: (id:Patient['id']) => void
-    updatePatient: (data:DraftPatient) => void
+  patients: Patient[],
+  activeId: Patient['id']
+  addPatient: (data: DraftPatient) => void
+  deletePatient: (id: Patient['id']) => void
+  getPatientById: (id: Patient['id']) => void
+  updatePatient: (data: DraftPatient) => void
 }
 
 //Esta funcion transforma un DraftPatient (Sin ID) en un Patient completo agregandole un ID con uuidv4
-const createPatient = (patient:DraftPatient):Patient => {
-    return {
-        ...patient,
-        id: uuidv4()
-    }
+const createPatient = (patient: DraftPatient): Patient => {
+  return {
+    ...patient,
+    id: uuidv4()
+  }
 }
 
 //Creo y exporto un hook personalizado usePatientStore para modificar/leer el estado en todos los componentes
 export const usePatientStore = create<PatientState>()(
-    devtools((set) => ({
+  devtools(
+    persist((set) => ({
+      patients: [],
+      activeId: '',
 
-    patients: [],
-    activeId:'',
-    
-    addPatient: (data) => {
+      addPatient: (data) => {
         const newPatient = createPatient(data)
         set((state) => ({
-            patients:[...state.patients, newPatient]
+          patients: [...state.patients, newPatient]
         }))
-    },
+      },
 
-    deletePatient: (id) => {
+      deletePatient: (id) => {
         set((state) => ({
-            patients: state.patients.filter(patient => patient.id !== id)
+          patients: state.patients.filter(patient => patient.id !== id)
         }))
-    },
+      },
 
-    getPatientById: (id) => {
+      getPatientById: (id) => {
         set(() => ({
-            activeId: id
+          activeId: id
         }))
-    },
+      },
 
-    updatePatient:(data) => {
+      updatePatient: (data) => {
         set((state) => ({
-            patients: state.patients.map(patient => patient.id === state.activeId ? {id: state.activeId, ...data} : patient), 
-            activeId:''
-            
+          patients: state.patients.map(patient => patient.id === state.activeId ? { id: state.activeId, ...data } : patient),
+          activeId: ''
+
         }))
-    }
-})))
+      }
+    }),
+      {
+        name: 'patient-storage',
+      }
+    )
+  ))
